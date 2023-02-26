271437be5b7f939472f9fd240a297d28d04e30d1
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-27 04:19:58 +0900

    Initial Commit

---
+++ ShortTaskMemo/ShortTaskMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortTaskMemoApp.swift
+//  ShortTaskMemo
+//
+//  Created by 渡邊魁優 on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct ShortTaskMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
